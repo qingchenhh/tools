fae5a577e72c9b712cbf3caa3462709bdbae8471
清晨 <37011241+qingchenhh@users.noreply.github.com>
2023-03-07 17:33:29 +0800

    Add files via upload

--- fscan结果处理.py
+++ fscan结果处理.py
@@ -1,8 +1,15 @@
 import os
 import sys
 
+print()
+print("="*60)
+print("fscan扫描的结果内容太多的话，可以用该脚本来显示带有[+]的信息。")
+print("="*60)
+print()
+
 if len(sys.argv) < 2:
     print("请输入一个fscan结果文件！")
+    print("用法：python3 fscan处理结果.py result.txt")
     exit()
 elif not os.path.exists(sys.argv[1]):
     print("您输入的文件没有找到！")
