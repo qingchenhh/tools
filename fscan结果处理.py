0fb5ab88f9da360c9a02c126fc30ad35878209e2
清晨 <37011241+qingchenhh@users.noreply.github.com>
2023-03-07 17:26:09 +0800

    Add files via upload

---
+++ fscan结果处理.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+if len(sys.argv) < 2:
+    print("请输入一个IP地址或者一个IP列表文件！")
+    exit()
+elif not os.path.exists(sys.argv[1]):
+    print("您输入的文件没有找到！")
+    exit()
+
+file = sys.argv[1]
+
+with open(file,mode='r',encoding='utf-8') as f:
+    flag = 0
+    for i in f:
+        str = i.replace('\n','')
+        str1 = str.lstrip()
+        if str.startswith('[+]'):
+            print(str)
+            flag = 1
+        elif  flag and str1.startswith('[->]'):
+            print(str)
+        else:
+            flag = 0
