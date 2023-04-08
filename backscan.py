8320a21ec836e79a427f84f73942911c40ee6e31
清晨 <37011241+qingchenhh@users.noreply.github.com>
2023-04-08 11:25:52 +0800

    Add files via upload

---
+++ backscan.py
@@ -0,0 +1,17 @@
+exts = ['.zip','.rar','.7z','.gz','.tar','.tar.gz','.tar.bz2','.bz2','.sql','.sql.tar.gz','.sql.gz','.sql.bz2']
+file_name = ['www','temp','wwwroot','web','1','2022','2021','2023','0','root','data','database','back','backup','backups','admin','webapps','backupdata']
+paths = ['/','/admin/','/backup/','/data/','/DB/','/back/']
+count = 0
+for path in paths:
+    for ext in exts:
+        for name in file_name:
+            filename = path + name + ext
+            count += 1
+            print(filename)
+print(count)
+# for ext in exts:
+#     for name in file_name:
+#         filename = 'D:\\phpstudy_pro\\WWW\\back\\' + name + ext
+#         with open(filename,mode='w',encoding='utf-8') as f:
+#             f.write('')
+#         # print(filename)
